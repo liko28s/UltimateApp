1c60a7e0eb544baa9d43e21c010e6f5e945b2246
Ramiro Alvarez <liko28s@gmail.com>
2017-01-02 17:34:17 -0500

    - Un monton de cosas

---
+++ app/controllers/scoreboard.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+//TODO ordenar por puntos o pp o cualquier cosa
+export default Ember.Controller.extend({
+  //teams: Ember.computed.sort('model', 'sortDefinition'),
+  //sortDefinition: ['teams.length:asc']
+});
